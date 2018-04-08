2e5644236445d6ccdc7e60265aa52d175de50f31
Yusuke Mito <y310.1984@gmail.com>
2018-04-08 22:27:13 +0900

    Add graphql-errors and rescue ActiveRecord::RecordNotFound

--- app/graphql/class_api_test_schema.rb
+++ app/graphql/class_api_test_schema.rb
@@ -3,3 +3,9 @@ class ClassApiTestSchema < GraphQL::Schema
   query(Types::QueryType)
   use GraphQL::Guard.new
 end
+
+GraphQL::Errors.configure(ClassApiTestSchema) do
+  rescue_from ActiveRecord::RecordNotFound do |exception, obj, args, ctx|
+    ctx.add_error(GraphQL::ExecutionError.new(exception.message))
+  end
+end
