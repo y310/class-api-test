9e02104d00555137b2059727aacf17117b2bafcf
Yusuke Mito <y310.1984@gmail.com>
2018-04-11 00:32:28 +0900

    Add DateTime scalar

---
+++ app/graphql/types/date_time.rb
@@ -0,0 +1,15 @@
+class Types::DateTime < Types::BaseScalar
+  description 'ISO8601フォーマットの日時データ'
+
+  def self.coerce_input(input_value, context)
+    begin
+      Time.zone.parse(input_value)
+    rescue
+      raise GraphQL::CoercionError, "#{input_value.inspect} is not a valid DateTime"
+    end
+  end
+
+  def self.coerce_result(ruby_value, context)
+    ruby_value.iso8601
+  end
+end
--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -5,6 +5,7 @@ class Types::PostType < Types::BaseObject
   global_id_field :id
   field :title, String, null: false
   field :body, String, null: true
+  field :createdAt, Types::DateTime, null: false
 
   field :user, Types::UserType, null: false
   field :comments, Types::CommentType.connection_type, null: false
