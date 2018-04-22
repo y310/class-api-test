ddcd1aa744bcdd56d7f7daf4e9377e143d1a1329
Yusuke Mito <y310.1984@gmail.com>
2018-04-22 21:06:50 +0900

    Field with argument

--- app/graphql/types/objects/post_type.rb
+++ app/graphql/types/objects/post_type.rb
@@ -8,7 +8,14 @@ class Types::Objects::PostType < Types::Objects::BaseObject
   field :body, String, null: true
   field :createdAt, Types::Scalars::DateTime, null: false
   field :test, String, null: true, deprecation_reason: 'This field is deprecated'
+  field :argumentTest, Boolean, null: false do
+    argument :flag, Boolean, required: true
+  end
 
   field :user, Types::Objects::UserType, null: false, complexity: 5
   field :comments, Types::Objects::CommentType.connection_type, null: false, complexity: 5
+
+  def argument_test(flag:)
+    flag
+  end
 end
