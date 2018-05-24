85361593c985df4e68c8e83e87b29940ceed092a
Yusuke Mito <y310.1984@gmail.com>
2018-05-25 00:16:19 +0900

    Experimental: make first argument required

--- app/graphql/authorized_field.rb
+++ app/graphql/authorized_field.rb
@@ -9,6 +9,12 @@ class AuthorizedField < GraphQL::Schema::Field
   def to_graphql
     field_defn = super # Returns a GraphQL::Field
     field_defn.metadata[:guard] = @guard
+
+    if @connection
+      arg = argument :first, Integer, "first is always required to make sure limited number of items is fetched", required: true
+      field_defn.arguments['first'] = arg.to_graphql
+    end
+
     field_defn
   end
 end
